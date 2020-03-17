5b04226f37927d7f303212f5a7dc296e88faf886
Javier Umana <javierumanafs@gmail.com>
2020-03-17 19:10:56 -0400

    mvp

--- src/App.js
+++ src/App.js
@@ -1,48 +1,75 @@
 import React from 'react';
+import { createStore } from 'redux';
+import { carReducer } from './reducer/carReducer';
+import { connect } from 'react-redux';
 
 import Header from './components/Header';
 import AddedFeatures from './components/AddedFeatures';
 import AdditionalFeatures from './components/AdditionalFeatures';
 import Total from './components/Total';
+import { BUY_ITEM } from './actions/carActions';
 
-const App = () => {
-  const state = {
-    additionalPrice: 0,
-    car: {
-      price: 26395,
-      name: '2019 Ford Mustang',
-      image:
-        'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
-      features: []
-    },
-    additionalFeatures: [
-      { id: 1, name: 'V-6 engine', price: 1500 },
-      { id: 2, name: 'Racing detail package', price: 1500 },
-      { id: 3, name: 'Premium sound system', price: 500 },
-      { id: 4, name: 'Rear spoiler', price: 250 }
-    ]
-  };
+export const store = createStore(carReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
-  const removeFeature = item => {
-    // dispatch an action here to remove an item
-  };
+const App = props => {
+  console.log('aaaaaaaa', props)
+  // const state = {
+  //   additionalPrice: 0,
+  //   car: {
+  //     price: 26395,
+  //     name: '2019 Ford Mustang',
+  //     image:
+  //       'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
+  //     features: []
+  //   },
+  //   additionalFeatures: [
+  //     { id: 1, name: 'V-6 engine', price: 1500 },
+  //     { id: 2, name: 'Racing detail package', price: 1500 },
+  //     { id: 3, name: 'Premium sound system', price: 500 },
+  //     { id: 4, name: 'Rear spoiler', price: 250 }
+  //   ]
+  // };
 
-  const buyItem = item => {
-    // dipsatch an action here to add an item
-  };
+  //const removeFeature = item => {
+  // dispatch an action here to remove an item
+  //};
+
+  // const buyItem = item => {
+
+  // };
 
   return (
     <div className="boxes">
       <div className="box">
-        <Header car={state.car} />
-        <AddedFeatures car={state.car} />
+        <Header
+          car={props.car}
+
+        />
+        <AddedFeatures
+          car={props.car}
+        />
       </div>
       <div className="box">
-        <AdditionalFeatures additionalFeatures={state.additionalFeatures} />
-        <Total car={state.car} additionalPrice={state.additionalPrice} />
+        <AdditionalFeatures
+          additionalFeatures={props.additionalFeatures}
+        />
+        <Total
+          car={props.car}
+          additionalPrice={props.additionalPrice}
+
+        />
       </div>
     </div>
   );
 };
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    additionalPrice: state.additionalPrice,
+    car: state.car,
+    additionalFeatures: state.additionalFeatures
+  };
+};
+export default connect(mapStateToProps, { BUY_ITEM })(App);
+
